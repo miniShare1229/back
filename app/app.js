2ae75dc0503b0b62bc4369670f8482a31afab801
Polarvear <znzpzp13@naver.com>
2023-01-12 23:53:41 +0900

    add react

--- app/app.js
+++ app/app.js
@@ -7,12 +7,18 @@ const app = express();
 
 //라우팅
 const home = require("./src/routes/home");
+const path = require("path");
 
 //앱 세팅
-app.set("views", "./src/views");
+app.set("views", "./src/views/home/");
+
 app.set("view engine", "ejs");
+//app.set("view engine", "jsx");
+//app.engine("jsx", require("express-react-views").createEngine());
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.static(`${__dirname}/src/routes/asset/`));
+app.use(express.static(path.join(__dirname, "/src/routes/")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,6 +6,14 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
+/*router.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+*/
+router.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+//router.get("/login", ctrl.output.login);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
 
