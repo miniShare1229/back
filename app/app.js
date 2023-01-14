8c01194eec9482245c90eaf5b6542dff437ed2c7
wonder-H <soimar@naver.com>
2023-01-14 20:32:59 +0900

    프론트 html 랜더링 적용, 패키지 설치(cors)

--- app/app.js
+++ app/app.js
@@ -1,26 +1,32 @@
 "use strict";
 
-//모듈
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-
-//라우팅
-const home = require("./src/routes/home");
 const path = require("path");
 
+// 프론트, 서버 ajax 통신 cors 처리
+var cors = require('cors');
+app.use(cors());
+
+//모듈
+const bodyParser = require("body-parser");
+
+//라우팅
+// const home = require("./src/routes/home");
+
 //앱 세팅
-app.set("views", "./src/views/home/");
+app.set("views", "/src/views/front");
 
-app.set("view engine", "ejs");
-//app.set("view engine", "jsx");
-//app.engine("jsx", require("express-react-views").createEngine());
-
-app.use(express.static(`${__dirname}/src/public`));
-app.use(express.static(`${__dirname}/src/routes/asset/`));
-app.use(express.static(path.join(__dirname, "/src/routes/")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+// app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+
+// 프론트 html 랜더링
+app.use(express.static(path.join(__dirname, '/src/views/front')));
+
+// 프론트측이 url 라우팅 처리하도록 설정(SPA, CSR) 
+app.get('*', function (req, res){
+  res.sendFile(path.join(__dirname, '/src/views/front/index.html'));
+});
 
 module.exports = app;
