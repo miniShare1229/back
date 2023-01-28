dad23a6a03bacf89009ba59000c5171127fc1863
Polarvear <znzpzp13@naver.com>
2023-01-28 22:50:39 +0900

    로그인 완벽 구현

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const session = require("express-session");
 
 // 프론트, 서버 ajax 통신 cors 처리
 var cors = require("cors");
@@ -30,24 +31,36 @@ app.set("views", "/src/views/front");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+app.use(
+  // 미들웨어 적용
+  session({
+    httpOnly: true,
+    secure: true,
+    secret: "@mi@ni#",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+    },
+  })
+);
 
 // 프론트 html 랜더링
 app.use(express.static(path.join(__dirname, "/src/views/front")));
 
-// /test로 get 요청하면
-/*
 app.post("/register", (req, res) => {
-  // console.log("성공");
-  // console.log("res:::::", res);
-  console.log(req.body);
+  //console.log(req.body);
   const connection = mysql.createConnection(conn); // DB 커넥션 생성
   connection.connect(function (err) {
     if (err) throw err;
     //console.log("Connected");
     const sql = `INSERT INTO user (id, pwd, nickname, created_date) VALUES ('${req.body.id}','${req.body.pwd}','${req.body.nickname}','2022-01-03')`;
     connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
+      if (err) {
+        throw err;
+      }
+      console.log("회원가입 완료");
     });
   }); // DB 접속
 
@@ -57,41 +70,36 @@ app.post("/register", (req, res) => {
   // json형식 데이터로 응답
   res.json();
 });
-*/
+
 //로그인 기능
-app.post("/login", (req, res) => {
-  //const id = req.body.id;
-  // const pwd = req.body.pwd;
-  const connection = mysql.createConnection(conn); // DB 커넥션 생성
+app.post("/login", async (req, res) => {
+  console.log("로그인 실행");
+  const connection = mysql.createConnection(conn);
   connection.connect(function (err) {
     // if (err) throw err;'
-    connection.query("select id, pwd, nickname from user", (err, rows) => {
-      if (err) {
-        throw err;
-      } else {
-        for (let i = 0; i < rows.length; i++) {
-          if (req.body.id === rows[i].id && req.body.pwd === rows[i].pwd) {
-            console.log("ok");
-            return res.status(200).json({
-              code: 200,
-              message: "성공",
-            });
-          } else {
-            //else 시 로직작성
-            console.log("아이디 비밀번호를 다시확인해주세요");
-            // return res.status(200).json({
-            //   code: 200,
-            //   message: "성공",
-            // });
+    connection.query(
+      "select id, pwd, nickname from user where id=? and pwd=?",
+      [req.body.id, req.body.pwd],
+      (err, rows) => {
+        if (rows.length === 0) {
+          if (err) {
+            throw err;
           }
+          res.status(400).json({
+            code: 400,
+            message: "실패",
+          });
+        } else {
+          //console.log((req.session.user = rows[0]));
+          res.status(200).json({
+            code: 200,
+            message: "성공",
+            body: rows[0].constructor.nickname,
+          });
         }
-        //return res.send();
       }
-    });
-  }); // DB 접속
-
-  // json형식 데이터로 응답
-  //res.json();
+    );
+  });
 });
 
 // 프론트측이 url 라우팅 처리하도록 설정(SPA, CSR)
